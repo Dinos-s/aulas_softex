f809928cb441eefd6f8948b069f35c6b437b610e
Gabriel <gabriel02ramos@gmail.com>
2025-09-24 11:04:25 -0300

    Ex05 realizado

--- aula10/ex05/index.js
+++ aula10/ex05/index.js
@@ -2,19 +2,36 @@ class Funcionario {
     nome
     salario
 
+    constructor(nome, salario) {
+        this.nome = nome
+        this.salario = salario
+    }
+
     descricao() {
-        
+        return `Funcionario ${this.nome} recebe R$ ${this.salario.toFixed(2)}`   
     }
 }
 
 class Gerente extends Funcionario {
-    constructor(parameters) {
-        
+    departamento
+    constructor(nome, salario, departamento) {
+        super(nome, salario)
+        this.departamento = departamento
+    }
+
+    descricao() {
+        return `Gerente ${this.nome} do departamento ${this.departamento} recebe R$ ${this.salario.toFixed(2)}`
     }
 }
 
-const conta1 = new ContaBancaria()
-conta1.largura = 5
-conta1.altura = 3
+const fun1 = new Funcionario()
+const ger1 = new Gerente()
+fun1.nome = "João"
+fun1.salario = 2000.00
 
-console.log(conta1.area(100), conta1.verSaldo());
+ger1.nome = "Maria"
+ger1.salario = 5000.00
+ger1.departamento = "Vendas"
+
+console.log(fun1.descricao());
+console.log(ger1.descricao());
