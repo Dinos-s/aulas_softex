8cc417ef28431bc7e8e44a60a2c842864f71adb3
Gabriel <gabriel02ramos@gmail.com>
2025-10-10 21:20:12 -0300

    Contador

--- react_softex/src/App.jsx
+++ react_softex/src/App.jsx
@@ -1,3 +1,20 @@
+import { useState } from "react"
+import "./App.css";
+
 export default function App() {
-  return <h1 style={{fontFamily:'system-ui'}}>Novo Projeto rodando</h1>
+
+  const [count, setCount] = useState(0)
+
+  function increment() {
+    setCount(count + 1)
+  }
+
+  return (
+    <>
+      <h1>Hello, world!</h1>
+      <div className="card">
+        <button onClick={increment}>Incrementa: {count}</button>
+      </div>
+    </>
+  )
 }
